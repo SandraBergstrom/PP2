0685004e5debf3e1ba414b7a80b7c3c9c10616b4
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-02-14 14:23:21 +0000

    Add function structure. Add eventlistener.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let boxes = Array.from(document.getElementsByClassName("board-box"));
+    boxes = Array(9).fill(null);
+
+  });
+
+
+let currentPlayer = "X"
 
 
 function displayName() {
     let username = document.getElementById("uname-input").value;
     document.getElementById("displayName").innerHTML = username;
+}
+
+function isRobotosTurn() {
+
+}
+
+function clickBox() {
+
+}
+
+function isWin() {
+
+}
+
+function isDraw() {
+
+}
+
+function increaseScore() {
+    
 }
