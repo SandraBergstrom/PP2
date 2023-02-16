ee0cd5b1f956896750f178503dfa8175d59ce1b4
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-02-16 13:25:18 +0000

    Add function all characters. Add play again button

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,46 +13,46 @@ let boxes = document.querySelectorAll(".board-box");
 
 let currentPlayer = "X";
 let winCombos = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
 // Will get the username, place it on the board and show next section.
 enterNameBtn.addEventListener("click", displayName);
 
 function displayName() {
-    let username = document.getElementById("uname-input").value;
-    document.getElementById("displayName").innerHTML = username;
-    if (username.trim() == "") {
-        msgWin.textContent = `Please let me know your name!`;
-        msgWin.style.color = "#F6A38E";
-        setTimeout(eraseName, 3000);
-    } else {
-        let pickCharacter = document.querySelector("#character-area");
-        pickCharacter.style.display = "flex";
-    }
-   // setTimeout(noName, 2000);
+  let username = document.getElementById("uname-input").value;
+  document.getElementById("displayName").innerHTML = username;
+  if (username.trim() == "") {
+    msgName.textContent = `Please let me know your name!`;
+    msgName.style.color = "#F6A38E";
+    setTimeout(eraseName, 3000);
+  } else {
+    let pickCharacter = document.querySelector("#character-area");
+    pickCharacter.style.display = "flex";
+  }
 
-    function eraseName() {
-        msgWin.style.color = "white";
-    }
+  function eraseName() {
+    msgName.style.color = "white";
+  }
 }
 
 // Will pick the unicorn character and show the game section
 unicorn.addEventListener("click", letsPlayUnicorn);
 
 function letsPlayUnicorn() {
-    let board = document.querySelector("#game-section");
-    board.style.display = "flex";
-    
-    let playerIcon = document.querySelector("#display-character-icon");
-    playerIcon.innerHTML = ` 
+  let board = document.querySelector("#game-section");
+  board.style.display = "flex";
+  let gameMsg = document.querySelector("#game-msg");
+  gameMsg.style.display = "flex";
+  let playerIcon = document.querySelector("#display-character-icon");
+  playerIcon.innerHTML = ` 
         <img 
             src = "assets/images/unicorn.png" 
             alt = "unicorn icon" 
@@ -65,55 +65,110 @@ function letsPlayUnicorn() {
                 border-radius: 25%;
             "
         />
-    `
+    `;
 }
 
+// Will pick the teddy bear character and show the game section
+teddy.addEventListener("click", letsPlayTeddy);
 
-// Will show the game section
-letsPlayBtn.addEventListener("click", letsPlay);
-
-function letsPlay() {
-    let board = document.querySelector("#game-section");
-    board.style.display = "flex";
+function letsPlayTeddy() {
+  let board = document.querySelector("#game-section");
+  board.style.display = "flex";
+  let gameMsg = document.querySelector("#game-msg");
+  gameMsg.style.display = "flex";
+  let playerIcon = document.querySelector("#display-character-icon");
+  playerIcon.innerHTML = ` 
+        <img 
+            src = "assets/images/teddy-bear.png" 
+            alt = "teddy bear icon" 
+            style = "
+                width: 75px;
+                background-color: #FFF1AD;
+                border: none;
+                height: auto;
+                padding: 15px;
+                border-radius: 25%;
+            "
+        />
+    `;
 }
 
-/*
-First part of this function is very inspired from https://www.youtube.com/watch?v=GTLLnF30up4&t=501s and will 
-check if the the latest box that was checked will win
-*/
-function checkIfWin() {
-    winCombos.forEach(function (combination) {
-        let check = combination.every(
-            (idx) => boxes[idx].innerText.trim() == currentPlayer
-        );
-        if (check) {
-            msgWin.textContent = `The winner is ${currentPlayer}!`;
-            msgWin.style.color = "#F6A38E";
-            playAgain();
-            let winner = `${currentPlayer}`;
-            if (winner === "X") {
-                increasePlayerScore();
-            } else if (winner === "O") {
-                increaseRobotoScore();
-            }
-        }
-    });
+// Will pick the dragon bear character and show the game section
+dragon.addEventListener("click", letsPlayDragon);
+
+function letsPlayDragon() {
+  let board = document.querySelector("#game-section");
+  board.style.display = "flex";
+  let gameMsg = document.querySelector("#game-msg");
+  gameMsg.style.display = "flex";
+  let playerIcon = document.querySelector("#display-character-icon");
+  playerIcon.innerHTML = ` 
+        <img 
+            src = "assets/images/dragon.png" 
+            alt = "dragon icon" 
+            style = "
+                width: 75px;
+                background-color: #64A6BD;
+                border: none;
+                height: auto;
+                padding: 15px;
+                border-radius: 25%;
+            "
+        />
+    `;
 }
+
+// Will clear the board
+letsPlayBtn.addEventListener("click", playAgain);
 
 function playAgain() {
-    letsPlayBtn.innerText = "Play again!";
-    
+    letsPlayBtn.innerText = "Let's play!";
+    letsPlayBtn.style.backgroundColor = "white";
+    letsPlayBtn.style.color = "#F6A38E";
+    boxes.forEach(box => {
+        box.innerText = "";
+    })
+  }
+
+// Will check if the the latest box that was checked will win
+
+function checkIfWin() {
+  winCombos.forEach(function (combination) {
+    let check = combination.every(
+      (idx) => boxes[idx].innerText.trim() == currentPlayer
+    );
+    if (check) {
+        msg
+      /*msgWin.textContent = `The winner is ${currentPlayer}!`;
+      msgWin.style.color = "#F6A38E";*/
+        
+      //let playAgainBtn = document.querySelector("#lets-play-button");
+      letsPlayBtn.innerText = "Play again!"
+      letsPlayBtn.style.backgroundColor = "#F6A38E";
+      letsPlayBtn.style.borderRadius = "5px";
+      letsPlayBtn.style.padding = "10px 15px";
+      letsPlayBtn.style.color = "white";
+      let winner = `${currentPlayer}`;
+      if (winner === "X") {
+        increasePlayerScore();
+      } else if (winner === "O") {
+        increaseRobotoScore();
+      }
+    }
+  });
 }
 
-
+// Below functions will increase winners score
 function increasePlayerScore() {
-    let oldScore = parseInt(document.querySelector("#player-score").innerText);
-    document.querySelector("#player-score").innerText = ++oldScore;
+  let oldScore = parseInt(document.querySelector("#player-score").innerText);
+  document.querySelector("#player-score").innerText = ++oldScore;
 }
 
 function increaseRobotoScore() {
-    let oldScoreRoboto = parseInt(document.querySelector("#roboto-score").innerText);
-    document.querySelector("#roboto-score").innerText = ++oldScoreRoboto;
+  let oldScoreRoboto = parseInt(
+    document.querySelector("#roboto-score").innerText
+  );
+  document.querySelector("#roboto-score").innerText = ++oldScoreRoboto;
 }
 
 /* /function checkIfDraw() {
@@ -127,19 +182,20 @@ function increaseRobotoScore() {
 boxes.forEach((box) => box.addEventListener("click", boxCheck));
 
 function boxCheck() {
-    if (this.innerText.trim() != "") return;
-    this.innerText = currentPlayer;
-    checkIfWin();
-    currentPlayer = currentPlayer === "X" ? "O" : "X";
-    robotosTurn();
+  if (this.innerText.trim() != "") return;
+  this.innerText = currentPlayer;
+  checkIfWin();
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+  robotosTurn();
 }
 
+// Makes Roboto play
 function robotosTurn() {
-    let play = 0;
-    do {
-        play = Math.floor(Math.random() * 9);
-    } while (boxes[play].textContent !== "");
-    boxes[play].textContent = "O";
-    checkIfWin();
-    currentPlayer = currentPlayer === "O" ? "X" : "O";
+  let play = 0;
+  do {
+    play = Math.floor(Math.random() * 9);
+  } while (boxes[play].textContent !== "");
+  boxes[play].textContent = "O";
+  checkIfWin();
+  currentPlayer = currentPlayer === "O" ? "X" : "O";
 }
