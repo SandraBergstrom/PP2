75d6a4ae4688fbb93d8f90080901389a50af3ad0
SandraBergstrom <bergstrom_sandra@hotmail.com>
2023-02-16 10:28:54 +0000

    Center box text, try pick character

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,42 +13,68 @@ let boxes = document.querySelectorAll(".board-box");
 
 let currentPlayer = "X";
 let winCombos = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 ];
 
 // Will get the username, place it on the board and show next section.
 enterNameBtn.addEventListener("click", displayName);
 
 function displayName() {
-  let username = document.getElementById("uname-input").value;
-  document.getElementById("displayName").innerHTML = username;
-  if (username == "") {
-    msgWin.textContent = `Please let me know your name!`;
-    msgWin.style.color = "#F6A38E";
-    setTimeout(eraseName, 3000);
-  } else {
-    let pickCharacter = document.querySelector("#character-area");
-    pickCharacter.style.display = "flex";
-  }
-  setTimeout(noName, 2000);
-  function eraseName() {
-    msgWin.style.color = "white";
-  }
+    let username = document.getElementById("uname-input").value;
+    document.getElementById("displayName").innerHTML = username;
+    if (username == "") {
+        msgWin.textContent = `Please let me know your name!`;
+        msgWin.style.color = "#F6A38E";
+        setTimeout(eraseName, 3000);
+    } else {
+        let pickCharacter = document.querySelector("#character-area");
+        pickCharacter.style.display = "flex";
+    }
+    setTimeout(noName, 2000);
+
+    function eraseName() {
+        msgWin.style.color = "white";
+    }
 }
+
+// Will pick the unicorn character and show the game section
+/*unicorn.addEventListener("click", letsPlayUnicorn);
+
+function letsPlayUnicorn() {
+    let board = document.querySelector("#game-section");
+    board.style.display = "flex";
+    
+    let playerIcon = document.querySelector("#display-character-icon");
+    playerIcon.innerHTML = ` 
+        <img 
+            src = "assets/images/unicorn.png" 
+            alt = "unicorn icon" 
+            style = "
+                height: 75 px;
+                background-color: #FDB0D1;
+                border: none;
+                height: auto;
+                padding: 15px;
+                border-radius: 25%;
+            "
+        />
+    `
+}*/
+
 
 // Will show the game section
 letsPlayBtn.addEventListener("click", letsPlay);
 
 function letsPlay() {
-  let board = document.querySelector("#game-section");
-  board.style.display = "flex";
+    let board = document.querySelector("#game-section");
+    board.style.display = "flex";
 }
 
 /*
@@ -56,22 +82,28 @@ First part of this function is very inspired from https://www.youtube.com/watch?
 check if the the latest box that was checked will win
 */
 function checkIfWin() {
-  winCombos.forEach(function (combination) {
-    let check = combination.every(
-      (idx) => boxes[idx].innerText.trim() == currentPlayer
-    );
-    if (check) {
-      msgWin.textContent = `The winner is ${currentPlayer}!`;
-      msgWin.style.color = "#F6A38E";
-      let winner = `${currentPlayer}`;
-        if (winner === "X") {
-            increasePlayerScore();
-        } else if (winner === "O") {
-            increaseRobotoScore();
+    winCombos.forEach(function (combination) {
+        let check = combination.every(
+            (idx) => boxes[idx].innerText.trim() == currentPlayer
+        );
+        if (check) {
+            msgWin.textContent = `The winner is ${currentPlayer}!`;
+            msgWin.style.color = "#F6A38E";
+            playAgain();
+            let winner = `${currentPlayer}`;
+            if (winner === "X") {
+                increasePlayerScore();
+            } else if (winner === "O") {
+                increaseRobotoScore();
+            }
         }
-    }
-  });
+    });
 }
+
+function playAgain() {
+    letsPlayBtn.innerText = "Play again!";
+}
+
 
 function increasePlayerScore() {
     let oldScore = parseInt(document.querySelector("#player-score").innerText);
@@ -94,19 +126,19 @@ function increaseRobotoScore() {
 boxes.forEach((box) => box.addEventListener("click", boxCheck));
 
 function boxCheck() {
-  if (this.innerText.trim() != "") return;
-  this.innerText = currentPlayer;
-  checkIfWin();
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-  robotosTurn();
+    if (this.innerText.trim() != "") return;
+    this.innerText = currentPlayer;
+    checkIfWin();
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    robotosTurn();
 }
 
 function robotosTurn() {
-  let play = 0;
-  do {
-    play = Math.floor(Math.random() * 9);
-  } while (boxes[play].textContent !== "");
-  boxes[play].textContent = "O";
-  checkIfWin();
-  currentPlayer = currentPlayer === "O" ? "X" : "O";
+    let play = 0;
+    do {
+        play = Math.floor(Math.random() * 9);
+    } while (boxes[play].textContent !== "");
+    boxes[play].textContent = "O";
+    checkIfWin();
+    currentPlayer = currentPlayer === "O" ? "X" : "O";
 }
